454aca08b42935620d45ba762cf7f2e259daab3f
Sharvari Desai <desai.sharvari@gmail.com>
2017-01-28 21:39:59 -0600

    Adding config file with API credentials

---
+++ src/app/config.ts
@@ -0,0 +1,7 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class Config {
+  public static SPACE_ID: string = 'kr5v9a04zcon';
+  public static API_KEY: string = '35eb75101e3b8d43cf9c1a373694ba2b0da1cd78f2697c193e5082636f8b6dc5';
+}
